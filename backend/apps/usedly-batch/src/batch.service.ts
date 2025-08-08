79a7351db04de37aa38624d34fe0196a677170c7
nurik <>
2025-08-09 00:28:54 +0900

    fix: fix batch server

--- backend/apps/usedly-batch/src/batch.service.ts
+++ backend/apps/usedly-batch/src/batch.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Member } from 'apps/nestar-api/src/libs/dto/member/member';
-import { Property } from 'apps/nestar-api/src/libs/dto/property/property';
-import { MemberStatus, MemberType } from 'apps/nestar-api/src/libs/enums/member.enum';
-import { PropertyStatus } from 'apps/nestar-api/src/libs/enums/property.enum';
+import { Member } from 'apps/usedly-api/src/libs/dto/member/member';
+import { Property } from 'apps/usedly-api/src/libs/dto/property/property';
+import { MemberStatus, MemberType } from 'apps/usedly-api/src/libs/enums/member.enum';
+import { PropertyStatus } from 'apps/usedly-api/src/libs/enums/property.enum';
 import { Model } from 'mongoose';
 
 @Injectable()
